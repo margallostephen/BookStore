5fe1e8fb87cc173123476c968e53e0df3354ae35
margallostephen <spmargallo@gmail.com>
2024-04-11 01:39:36 +0800

    Add notification library

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 import store from './store'
+import Notifications from '@kyvg/vue3-notification'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Notifications).mount('#app')
