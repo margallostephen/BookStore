935811d1f19d7cad42f096a1083585da15a17d4a
margallostephen <spmargallo@gmail.com>
2024-04-12 18:01:30 +0800

    Add book list component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    name: 'books',
+    component: () => import('../views/BookList.vue')
+  },
+  {
+    path: '/login',
     name: 'login',
     component: () => import('../views/LoginPage.vue')
   },
